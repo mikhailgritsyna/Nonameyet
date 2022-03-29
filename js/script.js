af698df69ba1fb971756d34186b47c00ab3158d4
mikhailgritsyna <63459857+mikhailgritsyna@users.noreply.github.com>
2022-03-29 16:36:59 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+new Swiper('.swiper', {
+  //  autoHeight: true,
+   slidesPerView: 4.3,
+   loop: true,
+   
+  // autoplay: {
+  //   delay: 1000,
+
+  // }
+});
+new Swiper('.swiper_2', {
+  //  autoHeight: true,
+  slidesPerView: 3.8,
+  loop: true,
+
+  autoplay: {
+    delay: 2000,
+
+  }
+});
